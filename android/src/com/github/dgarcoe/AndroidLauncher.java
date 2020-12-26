be68e05a15387fb72058cb9ef358a993ab8066ab
dgarcoe <dannn86m@gmail.com>
2020-12-26 13:54:47 +0100

    Initial commit

---
+++ android/src/com/github/dgarcoe/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.github.dgarcoe;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.dgarcoe.MeetyMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MeetyMain(), config);
+	}
+}
---
+++ desktop/src/com/github/dgarcoe/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.dgarcoe.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.dgarcoe.MeetyMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MeetyMain(), config);
+	}
+}
